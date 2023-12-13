787ba2537e5aafd0417def404db46084982135a4
Ashutosh Mohanty <ashutoshmohanty3815@gmail.com>
2023-12-13 14:58:56 +0000

    Modified

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -28,7 +28,7 @@ export default async function OG() {
       >
         <img
           src={new URL(
-            "../public/apple-touch-icon.png",
+            "../public/neuramock.png",
             import.meta.url
           ).toString()}
           alt="NeuraMock Logo"
